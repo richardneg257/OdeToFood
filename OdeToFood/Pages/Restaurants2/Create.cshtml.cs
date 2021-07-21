ae72eafbe70a26872784afdd3d3da22ea1d829d9
Richard Oswaldo Negrón Gonzáles <richard.negron.257@gmail.com>
2021-07-21 03:42:07 -0500

    scaffolding crud

---
+++ OdeToFood/Pages/Restaurants2/Create.cshtml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using OdeToFood.Core;
+using OdeToFood.Data;
+
+namespace OdeToFood.Pages.Restaurants2
+{
+    public class CreateModel : PageModel
+    {
+        private readonly OdeToFood.Data.OdeToFoodDbContext _context;
+
+        public CreateModel(OdeToFood.Data.OdeToFoodDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Restaurant Restaurant { get; set; }
+
+        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Restaurants.Add(Restaurant);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
