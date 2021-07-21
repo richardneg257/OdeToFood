514f3cf58f8217e6321805421ab592088e3d48eb
Richard Oswaldo Negrón Gonzáles <richard.negron.257@gmail.com>
2021-07-21 17:54:17 -0500

    se creo controlador restaurantscontroller, se creo razor page cliente, se usa ajax, services.AddControllers

--- OdeToFood/Startup.cs
+++ OdeToFood/Startup.cs
@@ -31,6 +31,7 @@ namespace OdeToFood
             services.AddScoped<IRestaurantData, SqlRestaurantData>();
 
             services.AddRazorPages();
+            services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -57,6 +58,7 @@ namespace OdeToFood
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapRazorPages();
+                endpoints.MapControllers();
             });
         }
     }
